d987c1ea17fa747e4e6d943733ec1d8115aad219
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2023-01-23 21:55:02 +0800

    perf: unable to register with duplicate names and phone numbers

--- api/register.php
+++ api/register.php
@@ -82,46 +82,59 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
 
   $user_res = $conn->query("SELECT * FROM users WHERE username='$username' LIMIT 1");
-  if ($user_res->num_rows === 0) {
-    $code_res = $conn->query("SELECT code FROM users WHERE type='$type' ORDER BY id DESC LIMIT 1");
-    $num = 0;
-
-    if ($code_res->num_rows > 0) {
-      $code_obj = $code_res->fetch_object();
-      $num = intval(substr($code_obj->code, 1));
-    }
-
-    $numstr = strval(++$num);
-    while (strlen($numstr) < 2) $numstr = "0$numstr";
-    $code_prefix = '';
-    if ($type === 'customer') $code_prefix = 'C';
-    if ($type === 'farmer') $code_prefix = 'F';
-    if ($type === 'admin') $code_prefix = 'A';
-    $code = $code_prefix . $numstr;
-
-    $hash = password_hash($password, PASSWORD_BCRYPT);
-    $conn->query("INSERT INTO users (code, username, password, email, mobile, name, gender, birthday, addressstreet, addresspurok, addressbrgy, type, verified)
-                  VALUES ('$code', '$username', '$hash', '$email', '$mobile', '$name', '$gender', '$birthday', '$address_street', '$address_purok', '$address_brgy', '$type', $verified)");
-
-    if ($type === 'customer' && $verified === 0) {
-      $imgpath = __DIR__ . '/../userdata/ids';
-      $validid = $_FILES['valid-id'];
-      $userid = $conn->insert_id;
-      $ext = pathinfo($validid['name'], PATHINFO_EXTENSION);
-      $success = move_uploaded_file($validid['tmp_name'], "$imgpath/$userid.$ext");
-
-      if (!$success) {
-        $conn->query("DELETE FROM users WHERE id=$userid");
-        $result['message'] = 'Unable to upload ID';
-        die(json_encode($result));
-      }
-    }
-
-    $result['success'] = true;
-    $result['message'] = 'Registered successfully';
-  } else {
+  if ($user_res->num_rows > 0) {
     $result['message'] = 'Username already exists';
+    die(json_encode($result));
   }
+
+  $phone_res = $conn->query("SELECT * FROM users WHERE mobile='$mobile' LIMIT 1");
+  if ($phone_res->num_rows > 0) {
+    $result['message'] = 'Phone number already exists';
+    die(json_encode($result));
+  }
+
+  $name_res = $conn->query("SELECT * FROM users WHERE name='$name' LIMIT 1");
+  if ($name_res->num_rows > 0) {
+    $result['message'] = 'Name already exists';
+    die(json_encode($result));
+  }
+
+  $code_res = $conn->query("SELECT code FROM users WHERE type='$type' ORDER BY id DESC LIMIT 1");
+  $num = 0;
+
+  if ($code_res->num_rows > 0) {
+    $code_obj = $code_res->fetch_object();
+    $num = intval(substr($code_obj->code, 1));
+  }
+
+  $numstr = strval(++$num);
+  while (strlen($numstr) < 2) $numstr = "0$numstr";
+  $code_prefix = '';
+  if ($type === 'customer') $code_prefix = 'C';
+  if ($type === 'farmer') $code_prefix = 'F';
+  if ($type === 'admin') $code_prefix = 'A';
+  $code = $code_prefix . $numstr;
+
+  $hash = password_hash($password, PASSWORD_BCRYPT);
+  $conn->query("INSERT INTO users (code, username, password, email, mobile, name, gender, birthday, addressstreet, addresspurok, addressbrgy, type, verified)
+                VALUES ('$code', '$username', '$hash', '$email', '$mobile', '$name', '$gender', '$birthday', '$address_street', '$address_purok', '$address_brgy', '$type', $verified)");
+
+  if ($type === 'customer' && $verified === 0) {
+    $imgpath = __DIR__ . '/../userdata/ids';
+    $validid = $_FILES['valid-id'];
+    $userid = $conn->insert_id;
+    $ext = pathinfo($validid['name'], PATHINFO_EXTENSION);
+    $success = move_uploaded_file($validid['tmp_name'], "$imgpath/$userid.$ext");
+
+    if (!$success) {
+      $conn->query("DELETE FROM users WHERE id=$userid");
+      $result['message'] = 'Unable to upload ID';
+      die(json_encode($result));
+    }
+  }
+
+  $result['success'] = true;
+  $result['message'] = 'Registered successfully';
 }
 
 echo json_encode($result);
